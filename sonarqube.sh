bc6710030782f35740de47691376d7cd9c026556
iyalkeerthana <123946084+iyalkeerthana@users.noreply.github.com>
2023-11-03 07:48:29 +0400

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -3,3 +3,4 @@
     docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
 if we stop and start the server, our docker container goes failure mode.
 we need to run the container again to up the sonarqube UI mode.
+docker start sonarqube
